1ee1f712f76ef1e8f355ee94441dd1c2f85307db
Your Name <meejanursk@gmail.com>
2025-08-08 08:05:20 +0530

    fixed the handle

--- app/api/index.js
+++ app/api/index.js
@@ -18,7 +18,7 @@ export const postDesign = (newPost, token) => {
 };
 export const setUserHandle = (userId, handle, token) =>
   axios.post(
-  `${url}/handle`,
+  `${url}/user/handle`,
     { userId, handle },
     {
       headers: {
