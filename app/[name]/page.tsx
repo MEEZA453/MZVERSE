cc648de1dfd5785873031d3738e27adc6b9f26c4
Your Name <meejanursk@gmail.com>
2025-08-08 09:30:47 +0530

    changed style

--- app/[name]/page.tsx
+++ app/[name]/page.tsx
@@ -54,7 +54,7 @@ const { user } = useSelector((state: any) => state.auth);
 console.log(user);
 const { product, loading, error } = useSelector((state: any) => state.getProductOfUser || {});
   const [activeIndex, setActiveIndex] = useState(0)
-  const tabs = ['Assets', 'Favourait', 'About']
+  const tabs = ['Store', 'Moodboard']
 const handleClick = (path: string): void => {
   window.location.href = window.location.origin +'/AllAssets/' + path;
 };
@@ -68,42 +68,31 @@ console.log(product)
 <IoMenuOutline onClick={()=>logout()} className='  text-white  rounded-[2px]'  size={24}/>
       </div>
 {loading ? <Loading/> : <div> 
-      <div className='profile relative flex flex-col h-90 border-b border-[#4d4d4d] gap-3 mt-10 items-center justify-center w-screen'>
+      <div className='profile relative flex flex-col h-70 border-b border-[#4d4d4d] gap-3 mt-10 items-center justify-center w-screen'>
         <Image height = {300} width = {300}
           className='h-24 w-24 rounded-full bg-[#dadada] object-cover'
           src={user?.profile || '/image.png'}
           alt ='profile' 
         />
-        {/* <h5 style={{fontSize : '17px', lineHeight : -1 ,marginTop : 4}}>{user?.name}</h5> */}
-      {/* <p  style={{lineHeight : -0.3}}>{user?.bio}</p> */}
+      <h5   style={{lineHeight : -0.3}}>@{user?.handle}</h5>
+        {/* <p style={{fontSize : '16px', lineHeight : -1}}>{user?.bio}</p> */}
  
-        <div className='flex gap-1 mt-2'>
-          <button className='border rounded text-[14px] px-4 py-0.5'>Instagram</button>
-          <button className='bg-white text-black text-[14px] px-4 py-0.5 rounded'>Hire now</button>
+        <div className='flex gap-1 mt-2 px-5 w-screen'>
+          {tabs.map((tab , index)=>{
+            return     <button  onClick={() => setActiveIndex(index)}  className={`border ${index === activeIndex ? 'text-black bg-white': 'text-white'}  rounded text-[14px] w-full py-0.5`}>{tab}</button>
+          })}
+     
         </div>
 
-        <div className='w-screen absolute bottom-1 flex justify-around gap-3'>
-          {tabs.map((tab, index) => (
-            <h6
-              key={index}
-              onClick={() => setActiveIndex(index)}
-              className={`cursor-pointer ${index === activeIndex ? 'text-white':'text-[#767676]'} text-sm`}
-            >
-              {tab}
-            </h6>
-          ))}
-        </div>
+       
 
         {/* Slider */}
-        <div
-          className='h-[2px] w-[33.33vw] -translate-x-[33.33vw] bg-white absolute bottom-0 transition-transform duration-500'
-          style={{ transform: `translateX(${activeIndex * 33.33}vw)` }}
-        />
+    
       </div>
 
 { !loading ? <div className='w-[300vw] flex duration-500' style={{transform :`translate(${-activeIndex*100}vw)`}}>
 
-  <div className='assets w-screen h-90'>
+  <div className='assets w-screen h-full'>
      <div className='lg:grid-cols-5 grid-cols-2 grid'>
     {product?.map((product:any, index : number) => (
              <div
@@ -152,7 +141,7 @@ console.log(product)
            ))}
       </div> 
   </div>
-<div className='fav w-screen h-90'>
+<div className='fav border-l border-[#4d4d4d] w-screen h-full'>
      <div className='lg:grid-cols-5 grid-cols-2 grid'>
     {favourites?.map((product:any, index : number) => (
              <div
