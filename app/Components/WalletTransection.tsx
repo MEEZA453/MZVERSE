3d4fa46ea44161ce061718083ffedf012dc0e791
Your Name <meejanursk@gmail.com>
2025-09-23 18:31:03 +0530

    fix the error

--- app/Components/WalletTransection.tsx
+++ app/Components/WalletTransection.tsx
@@ -1,16 +1,25 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+interface WalletTransectionProps {
+  transaction: any;
+  buyer: any;
+  isLightMode: boolean;
+}
 
-export default function WalletTransection({transaction , buyer}){
+export default function WalletTransection({
+  transaction,
+  buyer,
+  isLightMode,
+}: WalletTransectionProps){
 
 
 const router = useRouter()
     return  <div
     
-                  className="w-full mb-1  px-3 border-b border-[#4d4d4d] items-center relative flex h-12 flex justify-between"
+                  className={`w-full mb-1  px-3 border-b  ${isLightMode ? 'border-[#dadada]':'border-[#4d4d4d]'} items-center relative flex h-12 flex justify-between`}
                 >
                   <div className="flex gap-2 items-center py-2">
-                    <div className="h-10 w-10 bg-[#1d1d1d] flex items-center">
+                    <div className={`h-10 w-10 ${isLightMode ? 'bg-[#dadada]':'bg-[#1d1d1d]'} flex items-center`}>
                       <Image
                                     onClick={() => router.push('/AllAssets/' + transaction?._id || '/')}
                         src={transaction?.image}
